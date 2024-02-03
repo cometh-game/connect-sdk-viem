80eec55aa34676083ecf17016bcefeb1dffd3ece
yoanslvy <97676023+yoanslvy@users.noreply.github.com>
2024-02-03 10:06:56 +0100

    refacto: sign message

--- src/wagmi/connector.ts
+++ src/wagmi/connector.ts
@@ -57,9 +57,12 @@ export class ComethConnectConnector extends Connector<
     chains,
     options: options_
   }: {
-    chains?: Chain[]
+    chains: Chain[]
     options: WagmiConfigConnectorParams
   }) {
+    if (chains.length !== 1)
+      throw new Error('Cometh Connect does not support multi network in config')
+
     const options = {
       shimDisconnect: true,
       ...options_
@@ -120,8 +123,7 @@ export class ComethConnectConnector extends Connector<
         await this.wallet.connect(localStorageAddress)
       } else {
         await this.wallet.connect()
-        const walletAddress = await this.wallet.getAddress()
-        window.localStorage.setItem('walletAddress', walletAddress)
+        window.localStorage.setItem('walletAddress', this.wallet.getAddress())
       }
     }
 
